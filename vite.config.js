4d0acdcaf7cb46abddafd58a01428facc1e86f4a
Monica Xu <mokaixu@gmail.com>
2026-01-15 01:29:44 -0500

    fix config for deploying

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/creative-garden/',
+  base: process.env.NODE_ENV === 'production' ? '/creative-garden/' : '/',
 })
 
