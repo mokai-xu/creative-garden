59e2bc686a1be21467e07e2de73cb2ff68b3c66b
Monica Xu <mokaixu@gmail.com>
2026-02-05 22:46:45 -0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pages serves at https://<user>.github.io/creative-garden/
-export default defineConfig({
-  plugins: [react()],
-  base: '/creative-garden/',
-})
